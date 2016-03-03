70ce797257104a0633f60b3265688548ffa106db
pankajmishra <seepankaj.mishra@gmail.com>
2016-03-03 14:40:34 +0530

    REST

---
+++ RESTfulExample/src/main/java/com/mkyong/rest/HelloWorldService.java
@@ -0,0 +1,24 @@
+package com.mkyong.rest;
+ 
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+ 
+@Path("/hello")
+public class HelloWorldService {
+ 
+	@PUT
+	@Path("/{param}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Response getMsg(@PathParam("param") String msg) {
+ 
+		String output = "Jersey say : " + msg;
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+ 
+}
